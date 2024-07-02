f809d8cce392e81f70e17faa5b6ff49610a709f7
dzeko003 <berenismassamba@gmail.com>
2024-07-02 12:23:56 +0200

    dernier commit

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -1,6 +1,6 @@
 import { GridColDef } from "@mui/x-data-grid";
 import "./add.scss";
-// import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type Props = {
   slug: string;
@@ -12,38 +12,44 @@ const Add = (props: Props) => {
 
   // TEST THE API
 
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-  // const mutation = useMutation({
-  //   mutationFn: () => {
-  //     return fetch(`http://localhost:8800/api/${props.slug}s`, {
-  //       method: "post",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         id: 111,
-  //         img: "",
-  //         lastName: "Hello",
-  //         firstName: "Test",
-  //         email: "testme@gmail.com",
-  //         phone: "123 456 789",
-  //         createdAt: "01.02.2023",
-  //         verified: true,
-  //       }),
-  //     });
-  //   },
-  //   onSuccess: () => {
-  //     queryClient.invalidateQueries([`all${props.slug}s`]);
-  //   },
-  // });
+  const mutation = useMutation({
+    mutationFn: () => {
+      return fetch(`http://localhost:8800/api/${props.slug}s`, {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: 111,
+          img: "",
+          lastName: "Hello",
+          firstName: "Test",
+          email: "testme@gmail.com",
+          phone: "123 456 789",
+          createdAt: "01.02.2023",
+          verified: true,
+        }),
+      });
+
+    },
+    onSuccess: () => {
+      if (props.slug) {
+        queryClient.invalidateQueries({ queryKey: [`all${props.slug}s`] });
+      } else {
+        console.error("Invalid props.slug:", props.slug);
+      }
+    },
+    
+  });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     //add new item
-    // mutation.mutate();
+    mutation.mutate();
     props.setOpen(false)
   };
   return (
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -4,6 +4,7 @@ import "./Users.scss";
 import { useState } from "react";
 import Add from "../../components/add/Add";
 import { userRows } from "../../data";
+import { useQuery } from "@tanstack/react-query";
 // import { useQuery } from "@tanstack/react-query";
 
 const columns: GridColDef[] = [
@@ -59,13 +60,13 @@ const Users = () => {
 
   // TEST THE API
 
-  // const { isLoading, data } = useQuery({
-  //   queryKey: ["allusers"],
-  //   queryFn: () =>
-  //     fetch("http://localhost:8800/api/users").then(
-  //       (res) => res.json()
-  //     ),
-  // });
+  const { isLoading, data } = useQuery({
+    queryKey: ["allusers"],
+    queryFn: () =>
+      fetch("http://localhost:8800/api/users").then(
+        (res) => res.json()
+      ),
+  });
 
   return (
     <div className="users">
@@ -73,14 +74,15 @@ const Users = () => {
         <h1>Users</h1>
         <button onClick={() => setOpen(true)}>Add New User</button>
       </div>
-      <DataTable slug="users" columns={columns} rows={userRows} />
+      {/* <DataTable slug="users" columns={columns} rows={userRows} /> */}
       {/* TEST THE API */}
 
-      {/* {isLoading ? (
+      {isLoading ? (
         "Loading..."
       ) : (
         <DataTable slug="users" columns={columns} rows={data} />
-      )} */}
+      )}
+
       {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   );
